4d02ccc223ed2a2a4618b1013bf81b2e7bf0a23b
chloe <marin.ducarme@gmail.com>
2024-01-16 20:07:34 +0100

    profilepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -6,13 +6,33 @@ import Navbar from "./components/Navbar";
 import ProfilePage from "./pages/auth/ProfilePage";
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      const fetchUser = async () => {
+        try {
+          const response = await fetch('');
+          const userData = await response.json();
+  
+          setUser(userData);
+          setLoading(false);
+        } catch (error) {
+          console.error('Error fetching user data:', error);
+          setLoading(false);
+        }
+      };
+  
+      fetchUser();
+    }, []);
+  
 
-  return (
-    <>
-      <Navbar />
-    </>
-  );
+    return (
+      <>
+        <ProfilePage user={user} />
+        <Navbar />
+      </>
+    );
 }
 
 export default App;
