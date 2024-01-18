9a8d0edfcb36b1eafefa5ccaffb47fbe8c67729e
chloe <marin.ducarme@gmail.com>
2024-01-18 20:31:05 +0100

    chloe

--- client/src/components/CreateCV/Experience.jsx
+++ client/src/components/CreateCV/Experience.jsx
@@ -4,21 +4,77 @@ function Experience() {
   const [experienceArray, setExperienceArray] = useState([]);
   const [experience, setExperience] = useState([]);
   const [profession, setProfession] = useState();
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const [tasks, setTasks] = useState();
+  const [city, setCity] = useState();
+  const [country, setCountry] = useState();
 
   const handleSubmit = () => {
-    setExperience({ profession: profession });
-    setExperienceArray([...experienceArray, experience]);
+    const newExperience = {
+      profession: profession,
+      startDate: startDate,
+      endDate: endDate,
+      tasks: tasks,
+      city: city,
+      country: country
+    };
+  
+    setExperienceArray([...experienceArray, newExperience]);
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={(e) => {
+    e.preventDefault();
+    handleSubmit();
+  }}>
         <label htmlFor="profession">Profession:</label>
         <input
           type="text"
           name="profession"
           onChange={(e) => {
             setProfession(e.target.value);
+          }}
+        />
+        <label htmlFor="startDate">Start Date:</label>
+        <input
+          type="date"
+          name="date"
+          onChange={(e) => {
+            setStartDate(e.target.value);
+          }}
+        />
+         <label htmlFor="endDate">End Date:</label>
+        <input
+          type="text"
+          name="text"
+          onChange={(e) => {
+            setEndDate(e.target.value);
+          }}
+        />
+          <label htmlFor="tasks">Tasks:</label>
+        <input
+          type="text"
+          name="text"
+          onChange={(e) => {
+            setTasks(e.target.value);
+          }}
+        />
+         <label htmlFor="city">City:</label>
+        <input
+          type="text"
+          name="text"
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+        />
+         <label htmlFor="country">Country:</label>
+        <input
+          type="text"
+          name="text"
+          onChange={(e) => {
+            setCountry(e.target.value);
           }}
         />
         <button type="submit">Next</button>
