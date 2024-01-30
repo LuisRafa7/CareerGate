46ac347742e030fc8068e979ed85988d1e30a386
chloe <marin.ducarme@gmail.com>
2024-01-30 20:47:06 +0100

    done

--- client/src/components/CarouselPage.jsx
+++ client/src/components/CarouselPage.jsx
@@ -13,7 +13,7 @@ function CarouselPage() {
             objectFit: 'cover',
           }}/>
         <Carousel.Caption className="centered-content">
-          <h3>First slide label</h3>
+          <h3 style={{ marginBottom: '90px', fontSize: '80px', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>Experience</h3>
           <div className="centered-buttons">
             <button
               className="button button-homepage"
@@ -24,7 +24,7 @@ function CarouselPage() {
               Create your CV
             </button>
           </div>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <p style={{ marginTop: '30px', fontSize: '50px', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -34,7 +34,7 @@ function CarouselPage() {
             objectFit: 'cover',
           }}/>
         <Carousel.Caption className="centered-content"> 
-          <h3>Second slide label</h3>
+          <h3 style={{ marginBottom: '90px', fontSize: '80px', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>Education</h3>
           <button className="button button-homepage"
       onClick={() => {
         navigate("/person");
@@ -42,7 +42,7 @@ function CarouselPage() {
     >
       Create your CV
     </button>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p style={{ marginTop: '30px', fontSize: '50px', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -52,7 +52,7 @@ function CarouselPage() {
             objectFit: 'cover',
           }}/>
         <Carousel.Caption className="centered-content">
-          <h3>Third slide label</h3>
+          <h3 style={{ marginBottom: '90px', fontSize: '80px', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>Languages</h3>
           <button className="button button-homepage"
       onClick={() => {
         navigate("/person");
@@ -60,7 +60,27 @@ function CarouselPage() {
     >
       Create your CV
     </button>
-          <p>
+          <p style={{ marginTop: '30px', fontSize: '50px', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={Image}   style={{
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+          }}/>
+        <Carousel.Caption className="centered-content">
+          <h3 style={{ marginBottom: '90px', fontSize: '80px', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>Skills</h3>
+          <button className="button button-homepage"
+      onClick={() => {
+        navigate("/person");
+      }}
+    >
+      Create your CV
+    </button>
+          <p style={{ marginTop: '30px', fontSize: '50px', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
         </Carousel.Caption>
